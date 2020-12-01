69703d9671ab0ffc22914eecc06870209954967f
Mehul Chaudhari <chaudharimehul2001@gmail.com>
2020-12-01 22:29:07 +0530

    Commit1

---
+++ myQuiz.js
@@ -0,0 +1,38 @@
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May I have your name? ');
+
+var score =0;
+
+console.log('Welcome ' + userName + " Let's see, How well do you know me?");
+
+function letssee(question,answer){
+  var userAnswer = readlineSync.question(question);
+
+  if(userAnswer == answer){
+    console.log("Correct! Thats my bro");
+    console.log("You earned One Point");
+    score++
+  }else{
+    console.log("duhh, That was so simple");
+    console.log("No point for this");
+  }
+
+  console.log("Current Score:" , score);
+  console.log("-*-*-*-*-*-*-*-*-*-*");
+
+}
+var questions = [{
+  question: "What's my favourite Cuisine? ",
+  answer: "Indian"
+}, {
+  question: "what's my pet name? ",
+  answer:"Anu"
+}, {
+  question:"My favourite SuperHero? ",
+  answer:"Iron Man"
+}];
+
+for (var i=0; i<questions.length; i++){
+  var currentQuestion = questions[i];
+  letssee(currentQuestion.question, currentQuestion.answer)
+}
